6e9c5069855a36fd81b573f85e6a4ad38e539ef2
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-28 17:44:19 +0700

    get port from env var

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"os"
+
 	"github.com/krifik/groupchat-be/app"
 	"github.com/krifik/groupchat-be/exception"
 )
 
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
 func main() {
 	appl := app.InitializedApp()
 	// app.InitializedPusher()
 	// Start App
-	err := appl.Listen("0.0.0.0:3000")
+	err := appl.Listen(getPort())
 	exception.PanicIfNeeded(err)
 }
